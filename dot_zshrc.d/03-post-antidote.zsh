a3c41e726c24b633b3761cc53708394fb312da1a
Ben Marden <benvmarden@gmail.com>
2022-09-10 17:39:20 -0700

    Add powerlevel10k config

--- dot_zshrc.d/03-post-antidote.zsh
+++ dot_zshrc.d/03-post-antidote.zsh
@@ -12,3 +12,7 @@ bindkey -v  # Vi keybindings
 # Set your prompt
 autoload -Uz promptinit && promptinit
 prompt powerlevel10k
+
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
