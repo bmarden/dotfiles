249c5f5f9c4648fd6dd99e7d8333a9b00726733d
Ben Marden <benvmarden@gmail.com>
2023-01-02 12:27:23 -0800

    Use lts for adsf nodejs version Add httpie to brew package list

--- .chezmoiscripts/run_once_before_03_setup-asdf.sh
+++ .chezmoiscripts/run_once_before_03_setup-asdf.sh
@@ -3,8 +3,8 @@
 ## Setup and install asdf plugins ##
 # nodejs
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf install nodejs latest
-asdf global nodejs latest
+asdf install nodejs lts
+asdf global nodejs lts
 
 # golang
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
