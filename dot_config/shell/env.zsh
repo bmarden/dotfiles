d48211fd5331110e6ddf13e69921d58d535f41b2
Ben Marden <benvmarden@gmail.com>
2023-03-01 21:52:29 -0800

    Setup ssh key signing for gitconfig

--- dot_config/shell/env.zsh
+++ dot_config/shell/env.zsh
@@ -7,4 +7,4 @@ export PNPM_HOME="$XDG_DATA_HOME/pnpm"
 
 # Add vars to path
 path=($PNPM_HOME $path)
-path+=($GOPATH/bin $GOROOT/bin)
+path+=($GOPATH/bin $GOROOT/bin $(brew --prefix)/opt/postgresql@15/bin)
