89d0ad5721313b91fdc90ce66b3bb1a8b3d372cf
Ben Marden <benvmarden@gmail.com>
2024-04-21 23:09:55 -0700

    Update homebrew installation and configuration

--- .install-homebrew.sh
+++ .install-homebrew.sh
@@ -6,3 +6,6 @@ type brew >/dev/null 2>&1 && exit
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # Set up homebrew in the current shell
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Update homebrew
+brew update
