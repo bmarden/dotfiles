ab591f3ddf6bc9e5209d7f8d4e2a6405bd3cbfda
Ben Marden <benvmarden@gmail.com>
2024-04-21 23:00:50 -0700

    Add hook to install homebrew

---
+++ .install-homebrew.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install homebrew if it's not already installed
+type brew >/dev/null 2>&1 && exit
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Set up homebrew in the current shell
+eval "$(/opt/homebrew/bin/brew shellenv)"
