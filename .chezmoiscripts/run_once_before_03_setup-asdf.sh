1b3711d497817e3238415766cc63e2cf294ff59a
Ben Marden <benvmarden@gmail.com>
2024-04-21 21:35:16 -0700

    Update iterm config and asdf scripts

--- .chezmoiscripts/run_once_before_03_setup-asdf.sh
+++ .chezmoiscripts/run_once_before_03_setup-asdf.sh
@@ -8,33 +8,31 @@
 if [ -n "$(command -v asdf)" ]; then
   source "$(brew --prefix asdf)/libexec/asdf.sh"
   export ASDF_DATA_DIR="$HOME/.local/share/asdf"
-  export ASDF_NPM_DEFAULT_PACKAGES_FILE="$ASDF_DATA_DIR/.default-npm-packages"
+  export ASDF_NPM_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME/asdf/default-npm-packages"
 else
   echo "asdf not installed. Skipping asdf setup."
   exit 0
 fi
 
 ## Setup and install asdf plugins ##
-plugins=("golang" "nodejs" "python" "ruby" "dotnet")  # List of plugins to check and install
+declare -A plugins
+plugins[nodejs]="20.12.2"
+plugins[java]="zulu-17.46.19"
+plugins[ruby]="3.2.2"
+plugins[python]="3.12.1"
+plugins[cocoapods]="1.15.2"
+plugins[golang]="1.22.2"
 
-# Iterate over each plugin
-for plugin in "${plugins[@]}"; do
+# Add each plugin, install AH version and set global
+for plugin in "${!plugins[@]}"; do
   echo "Adding $plugin..."
   asdf plugin-add "$plugin"
 
-  # Construct the env variable name
-  plugin_upper=$(echo "$plugin" | tr '[:lower:]' '[:upper:]')
-  asdf_plugin_version="ASDF_${plugin_upper}_VERSION"
-  
-  if [ -z "${!asdf_plugin_version}" ]; then
-
-    echo "No version set for plugin $plugin. Skipping..."
-    continue
-  fi
+  ah_plugin_version="${plugins[$plugin]}"
 
   echo "Installing AH version for plugin $plugin..."
-  asdf install "$plugin" "${!asdf_plugin_version}"
+  asdf install "$plugin" "$ah_plugin_version"
 
   echo "Setting global version for $plugin..."
-  asdf global "$plugin" "${!asdf_plugin_version}"
+  asdf global "$plugin" "$ah_plugin_version"
 done
