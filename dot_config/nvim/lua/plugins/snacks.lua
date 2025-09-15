6042e768f502e9ea4a16cdeb5d357f6097ed7eeb
Ben Marden <benvmarden@gmail.com>
2025-09-15 14:20:27 -0700

    Add conform and nvim-lint configs - fix snacks

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -4,6 +4,19 @@ return {
     "folke/snacks.nvim",
     ---@type snacks.Config
     opts = {
+      dashboard = {
+        preset = {
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.picker.files()" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.picker.grep()" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.picker.recent()" },
+            { icon = " ", key = "p", desc = "Projects", action = ":lua Snacks.picker.projects()" },
+            { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+        },
+      },
       explorer = {
         replace_netrw = true,
       },
