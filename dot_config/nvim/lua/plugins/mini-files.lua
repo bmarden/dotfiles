9c17f95bd7685cc90dd68e8765f29edbabfa9d25
Ben Marden <benvmarden@gmail.com>
2025-09-22 18:43:38 -0700

    Add mini files configuration

---
+++ dot_config/nvim/lua/plugins/mini-files.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-mini/mini.files",
+    opts = {
+      windows = {
+        preview = true,
+        width_focus = 50,
+        width_preview = 80,
+      },
+      options = {
+        -- Whether to use for editing directories
+        -- Disabled by default in LazyVim because neo-tree is used for that
+        use_as_default_explorer = false,
+      },
+    },
+  },
+}
