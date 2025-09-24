ccede98e22bfe224593f936cccce27dc167d9313
Ben Marden <benvmarden@gmail.com>
2025-09-24 10:57:59 -0700

    fix: Maybe fix for gopls erroring on Octo diff view

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -6,36 +6,10 @@ return {
         settings = {
           gopls = {
             gofumpt = false,
-            codelenses = {
-              gc_details = false,
-              generate = true,
-              regenerate_cgo = true,
-              run_govulncheck = true,
-              test = true,
-              tidy = true,
-              upgrade_dependency = true,
-              vendor = true,
-            },
-            hints = {
-              assignVariableTypes = true,
-              compositeLiteralFields = true,
-              compositeLiteralTypes = true,
-              constantValues = true,
-              functionTypeParameters = true,
-              parameterNames = true,
-              rangeVariableTypes = true,
-            },
             analyses = {
-              nilness = true,
-              unusedparams = true,
-              unusedwrite = true,
-              useany = true,
               ST1003 = false,
             },
-            usePlaceholders = true,
-            completeUnimported = true,
             staticcheck = false,
-            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
             semanticTokens = true,
           },
         },
@@ -45,7 +19,14 @@ return {
       gopls = function(_, opts)
         -- workaround for gopls not supporting semanticTokensProvider
         -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
-        LazyVim.lsp.on_attach(function(client, _)
+        LazyVim.lsp.on_attach(function(client, bufnr)
+          -- Detach gopls from octo:// buffers
+          local buf_name = vim.api.nvim_buf_get_name(bufnr)
+          if string.match(buf_name, "^octo://") then
+            vim.lsp.buf_detach_client(bufnr, client.id)
+            return
+          end
+
           if not client.server_capabilities.semanticTokensProvider then
             local semantic = client.config.capabilities.textDocument.semanticTokens
             client.server_capabilities.semanticTokensProvider = {
