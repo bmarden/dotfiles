904650e424003f60883689f19311e4bbd65a99e6
Ben Marden <benvmarden@gmail.com>
2025-10-17 14:29:28 -0700

    Add nil check in nvim-lspconfig and disable claudecode extension for now

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -32,17 +32,17 @@ return {
       },
     },
     setup = {
-      gopls = function(_, opts)
+      gopls = function(_, _)
         -- workaround for gopls not supporting semanticTokensProvider
         -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
-        LazyVim.lsp.on_attach(function(client, bufnr)
+        LazyVim.lsp.on_attach(function(client, _)
           if not client.server_capabilities.semanticTokensProvider then
             local semantic = client.config.capabilities.textDocument.semanticTokens
             client.server_capabilities.semanticTokensProvider = {
               full = true,
               legend = {
-                tokenTypes = semantic.tokenTypes,
-                tokenModifiers = semantic.tokenModifiers,
+                tokenTypes = semantic and semantic.tokenTypes or {},
+                tokenModifiers = semantic and semantic.tokenModifiers or {},
               },
               range = true,
             }
