8cf64a888b37946cbbc190963653b115e4b8c8de
Ben Marden <benvmarden@gmail.com>
2025-11-29 23:53:46 -0800

    Hide DS_Store files in snacks

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -41,7 +41,7 @@ return {
           explorer = {
             hidden = true,
             ignored = true,
-            exclude = { ".git", "node_modules" },
+            exclude = { ".git", "node_modules", ".DS_Store" },
             win = {
               list = {
                 keys = {
