d15dd5e9c9f1c4590c165a93bd37503ea61d40b0
Ben Marden <benvmarden@gmail.com>
2026-01-21 12:51:22 -0800

    Fix markdownlint cmd and cleanup other plugins

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -4,11 +4,7 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      keymap = {
-        preset = "enter",
-        ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
-        ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
-      },
+      keymap = {},
       completion = {
 
         list = {
@@ -17,12 +13,6 @@ return {
             auto_insert = true,
           },
         },
-        -- ghost_text = {
-        --   enabled = true,
-        -- },
-        -- menu = {
-        --   auto_show = false,
-        -- },
       },
       enabled = function()
         return not vim.tbl_contains({ "tex" }, vim.bo.filetype) and vim.bo.buftype ~= "prompt"
--- dot_config/nvim/lua/plugins/nvim-lint.lua
+++ dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -4,10 +4,11 @@ return {
     linters_by_ft = {
       go = { "golangcilint" },
       sql = { "sqlfluff" },
+      markdown = { "markdownlint-cli2" },
     },
     linters = {
       ["markdownlint-cli2"] = {
-        args = { "--config", vim.fn.expand("$HOME/.markdownlint-cli2.yaml"), "--" },
+        prepend_args = { "--config", "/Users/bmarden/.markdownlint-cli2.yaml", "--" },
       },
       tflint = {
         args = { "--config", vim.fn.expand("$HOME/.tflint.hcl") },
