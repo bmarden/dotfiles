eb73d4da37b8912bb0a837e19aa15a3ad08f349e
Ben Marden <benvmarden@gmail.com>
2026-01-27 16:07:54 -0800

    Remove overridden codediff settings

--- dot_config/nvim/lua/plugins/codediff.lua
+++ dot_config/nvim/lua/plugins/codediff.lua
@@ -8,8 +8,6 @@ return {
       view = {
         quit = "q", -- Close diff tab
         toggle_explorer = "<leader>b", -- Toggle explorer visibility (explorer mode only)
-        next_hunk = "]h", -- Jump to next change
-        prev_hunk = "[h", -- Jump to previous change
         next_file = "]f", -- Next file in explorer mode
         prev_file = "[f", -- Previous file in explorer mode
       },
