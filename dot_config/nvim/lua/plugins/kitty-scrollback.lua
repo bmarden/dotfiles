5a1199a2c461d5bdeb7edae9a0b03c533d439313
Ben Marden <benvmarden@gmail.com>
2026-01-27 16:07:45 -0800

    Update kitty config and scrollback

--- dot_config/nvim/lua/plugins/kitty-scrollback.lua
+++ dot_config/nvim/lua/plugins/kitty-scrollback.lua
@@ -6,5 +6,16 @@ return {
   event = { "User KittyScrollbackLaunch" },
   version = "*", -- latest stable version, may have breaking changes if major version changed
   -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
-  opts = {},
+  config = function()
+    require("kitty-scrollback").setup({
+      myconfig = {
+        status_window = {
+          autoclose = true,
+        },
+        paste_window = {
+          yank_register_enabled = false,
+        },
+      },
+    })
+  end,
 }
