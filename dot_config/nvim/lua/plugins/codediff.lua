9029a56c50aa47b6d7f2bd789d2c1411f7012105
Ben Marden <benvmarden@gmail.com>
2026-01-28 11:51:34 -0800

    Update CodeDiff plugin to use latest changes

--- dot_config/nvim/lua/plugins/codediff.lua
+++ dot_config/nvim/lua/plugins/codediff.lua
@@ -1,16 +1,6 @@
 return {
   "esmuellert/codediff.nvim",
-  branch = "next",
   dependencies = { "MunifTanjim/nui.nvim" },
   cmd = "CodeDiff",
-  opts = {
-    keymaps = {
-      view = {
-        quit = "q", -- Close diff tab
-        toggle_explorer = "<leader>b", -- Toggle explorer visibility (explorer mode only)
-        next_file = "]f", -- Next file in explorer mode
-        prev_file = "[f", -- Previous file in explorer mode
-      },
-    },
-  },
+  opts = {},
 }
