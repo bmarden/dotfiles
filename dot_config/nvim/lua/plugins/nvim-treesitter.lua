da62f295431fec9389cade2c1054d42ee2ff475f
Ben Marden <benvmarden@gmail.com>
2026-01-28 16:06:19 -0800

    Enable desired dotfile configuration

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,19 @@
+---@type string
+local xdg_config = vim.env.XDG_CONFIG_HOME or vim.env.HOME .. "/.config"
+
+---@param path string
+local function have(path)
+  return vim.uv.fs_stat(xdg_config .. "/" .. path) ~= nil
+end
+
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
+    local function add(lang)
+      if type(opts.ensure_installed) == "table" then
+        table.insert(opts.ensure_installed, lang)
+      end
+    end
     -- add tsx and treesitter
     vim.list_extend(opts.ensure_installed, {
       "bash",
@@ -17,5 +30,31 @@ return {
       "tsx",
       "typescript",
     })
+
+    vim.filetype.add({
+      extension = { rasi = "rasi", rofi = "rasi", wofi = "rasi" },
+      filename = {
+        ["vifmrc"] = "vim",
+      },
+      pattern = {
+        [".*/waybar/config"] = "jsonc",
+        [".*/mako/config"] = "dosini",
+        [".*/hypr/.+%.conf"] = "hyprlang",
+        ["%.env%.[%w_.-]+"] = "sh",
+      },
+    })
+
+    add("git_config")
+    if have("hypr") then
+      add("hyprlang")
+    end
+
+    if have("fish") then
+      add("fish")
+    end
+
+    if have("rofi") or have("wofi") then
+      add("rasi")
+    end
   end,
 }
