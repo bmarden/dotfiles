cdc1620bfe8f77aabb9ff451d5f9c0df77258d91
Ben Marden <benvmarden@gmail.com>
2026-02-09 21:23:21 -0800

    Adjust color themes - using bamboo

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -27,10 +27,40 @@ return {
     },
   },
   {
-    "tiagovla/tokyodark.nvim",
-  },
-  {
     "rebelot/kanagawa.nvim",
+    opts = {
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none", -- Transparent gutter
+            },
+          },
+        },
+      },
+    },
+  },
+  -- Using lazy.nvim
+  {
+    "ribru17/bamboo.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      colors = {
+        bg0 = "#111317", -- Custom background color
+        -- bg0 = "#252623",
+      },
+      highlights = {
+        -- make comments blend nicely with background, similar to other color schemes
+        ["@comment"] = { fg = "$grey" },
+      },
+    },
+    -- config = function()
+    --   require("bamboo").setup({
+    --     -- optional configuration here
+    --   })
+    --   require("bamboo").load()
+    -- end,
   },
   -- {
   --   "navarasu/onedark.nvim",
@@ -41,12 +71,17 @@ return {
   -- },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
+  { "folke/tokyonight.nvim", opts = {
+    style = "night",
+  } },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "onedark",
-      colorscheme = "onedark_vivid",
+      -- colorscheme = "onedark_vivid",
+      -- colorscheme = "tokyonight",
+      colorscheme = "bamboo",
     },
   },
 }
