3c683334e3e672a00732e2622efbbca77ac93912
Ben Marden <benvmarden@gmail.com>
2026-03-30 09:53:28 -0700

    Disable image.nvim

--- dot_config/nvim/lua/plugins/image.lua
+++ dot_config/nvim/lua/plugins/image.lua
@@ -1,7 +1,8 @@
 return {
   "3rd/image.nvim",
   build = false,
-  enabled = vim.env.KITTY_SCROLLBACK_NVIM ~= "true",
+  -- enabled = vim.env.KITTY_SCROLLBACK_NVIM ~= "true",
+  enabled = false,
   opts = {
     backend = "kitty",
     processor = "magick_cli",
