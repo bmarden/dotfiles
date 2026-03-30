1fdc20ca2b3b168f189a05aefa74380500824037
Ben Marden <benvmarden@gmail.com>
2026-03-30 09:52:15 -0700

    Remove redundant filters when attaching lsp to a buffer

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -7,29 +7,12 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
--- Prevent gopls from attaching to octo://, diffview://, and diff mode buffers
 -- Prevent markdown_oxide from attaching to non-markdown files
 -- Wrap buf_attach_client to filter these out before attachment happens
 local _original_buf_attach_client = vim.lsp.buf_attach_client
 ---@diagnostic disable-next-line: duplicate-set-field
 vim.lsp.buf_attach_client = function(bufnr, client_id)
   local client = vim.lsp.get_client_by_id(client_id)
-
-  -- Only filter gopls
-  if client and client.name == "gopls" then
-    local bufname = vim.api.nvim_buf_get_name(bufnr)
-
-    -- Check for special URI schemes
-    if bufname:match("^octo://") or bufname:match("^diffview://") then
-      return false
-    end
-
-    -- Check if buffer is in diff mode
-    local winid = vim.fn.bufwinid(bufnr)
-    if winid ~= -1 and vim.wo[winid].diff then
-      return false
-    end
-  end
 
   -- Only allow markdown_oxide to attach to markdown files
   if client and client.name == "markdown_oxide" then
