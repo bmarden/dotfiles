6100198eba078ba65eb34fcd388068c9ee47f728
Ben Marden <benvmarden@gmail.com>
2026-04-21 10:01:09 -0700

    Fix claude todo from showing up in todo-comments

--- dot_config/nvim/lua/plugins/todo-comments.nvim.lua
+++ dot_config/nvim/lua/plugins/todo-comments.nvim.lua
@@ -2,8 +2,10 @@ return {
   "folke/todo-comments.nvim",
   opts = {
     search = {
-      -- pattern = [[\b(KEYWORDS):]], -- ripgrep regex }
-      pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+      pattern = [[\b(KEYWORDS)(\([^)]*\))?:]], -- allow optional (tag) before colon, e.g. TODO(human):
+    },
+    highlight = {
+      pattern = [[.*<(KEYWORDS)%(\([^)]*\))?\s*:]], -- Vim regex (very magic), allow optional (tag) before colon
     },
   },
 }
