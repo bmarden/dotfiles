80df8193da0867889972b1c2bfa0fdc3beffe6c3
Ben Marden <benvmarden@gmail.com>
2026-06-19 11:19:30 -0700

    Add keybinding for disabling markdown-plus

--- dot_config/nvim/lua/plugins/markdown-plus.lua
+++ dot_config/nvim/lua/plugins/markdown-plus.lua
@@ -1,8 +1,26 @@
+local opts = {}
+
+local enabled = true
+
+local function toggle()
+  local mp = require("markdown-plus")
+  enabled = not enabled
+  if enabled then
+    mp.setup(opts)
+    vim.notify("markdown-plus enabled", vim.log.levels.INFO)
+  else
+    mp.teardown()
+    vim.notify("markdown-plus disabled", vim.log.levels.WARN)
+  end
+end
+
 return {
   -- dir = "~/code-personal/markdown-plus.nvim",
   "yousefhadder/markdown-plus.nvim",
-  version = "*", -- latest stable version, may have breaking changes if major version changed
   ft = "markdown",
   enabled = true,
-  opts = {},
+  opts = opts,
+  keys = {
+    { "<leader>uM", toggle, desc = "Toggle markdown-plus" },
+  },
 }
