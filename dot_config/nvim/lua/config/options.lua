364eb97673066c89b6bffce53c2dab8c49358395
Ben Marden <benvmarden@gmail.com>
2026-07-15 22:05:39 -0700

    feat(nvim): add spell and substitution preview options

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -10,3 +10,9 @@ vim.g.lazyvim_prettier_needs_config = true
 vim.g.root_spec = { "cwd" }
 
 vim.opt.swapfile = false
+
+-- Make spell checking take into account camelCase words (e.g., "camelCase" is treated as two words: "camel" and "Case")
+vim.opt.spelloptions = "camel"
+
+-- Enable live preview of substitutions in a split window (e.g., :%s/foo/bar/)
+-- vim.opt.inccommand = "split"
